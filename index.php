751560a128dc54df3aea7fdcd9ffd9ca26c3631e
sigintlpinvest <85190474+sigintlpinvest@users.noreply.github.com>
2021-06-02 09:19:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
 echo "Hello LINE BOT";
+use LINE\LINEBot\EchoBot\Dependency;
+use LINE\LINEBot\EchoBot\Route;
+use LINE\LINEBot\EchoBot\Setting;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$setting = Setting::getSetting();
+$app = new Slim\App($setting);
+
+(new Dependency())->register($app);
+(new Route())->register($app);
+
+$app->run();
